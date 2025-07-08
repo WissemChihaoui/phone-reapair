535a6de48980a6973fd69e22291d2b32548a21e2
WissemChihaoui <chihahoui10@gmail.com>
2025-07-08 18:13:47 +0100

    start on dialog

--- src/sections/reparations2/document/forms/piece-form.jsx
+++ src/sections/reparations2/document/forms/piece-form.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { useFieldArray, useFormContext } from 'react-hook-form';
 
 import Grid from '@mui/material/Unstable_Grid2';
-import { Stack, Button, Typography } from '@mui/material';
+import { Stack, Button, Typography, Dialog, DialogTitle, DialogContent } from '@mui/material';
 
 import { Field } from 'src/components/hook-form';
 import { Iconify } from 'src/components/iconify';
+import { useBoolean } from 'src/hooks/use-boolean';
 
 export default function PieceForm({ index: formIndex, formId, onRemove }) {
   const { control } = useFormContext();
 
+  const open = useBoolean()
+
   return (
+    <>
     <Stack spacing={2}>
       <Typography variant="subtitle1">Piéce à changer</Typography>
 
@@ -22,7 +26,7 @@ export default function PieceForm({ index: formIndex, formId, onRemove }) {
             name={`products[${formIndex}].piece[${formIndex}].nom`}
             label="Pièce à changer / Article:"
           />
-          <Button variant="contained">+</Button>
+          <Button variant="contained" onClick={()=>open.onTrue()}>+</Button>
           </Stack>
         </Grid>
         <Grid xs={12} md={4}>
@@ -68,5 +72,18 @@ export default function PieceForm({ index: formIndex, formId, onRemove }) {
         </Grid>
       </Grid>
     </Stack>
+    <AddArticleDialog open={open.value} onClose={open.onFalse}/>
+    </>
   );
+}
+
+function AddArticleDialog({open, onClose}) {
+  return(
+  <Dialog open={open} onClose={onClose}>
+    <DialogTitle>Ajouter Article</DialogTitle>
+    <DialogContent>
+      Dialog
+    </DialogContent>
+  </Dialog>
+  )
 }
