ac1c6d5c55eefa24de566ecb6f70252b1b83f199
Wissem Chihaoui <chihahoui10@gmail.com>
2025-08-08 15:38:52 +0100

    fix language in dashboard

--- src/components/dashboard/search-reparation/SearchReparation.jsx
+++ src/components/dashboard/search-reparation/SearchReparation.jsx
@@ -13,7 +13,7 @@ export default function SearchReparation() {
     }}
   >
     <Box sx={{ flexGrow: 1 }}>
-      <Box sx={{ typography: 'body', textAlign: 'center', fontWeight:'700' }}>Chercher Réparation </Box>
+      <Box sx={{ typography: 'body', textAlign: 'center', fontWeight:'700' }}>Rechercher une réparation</Box>
       <Box sx={{ mt: 1.5, mb: 1, typography: 'h3', display: 'flex' }}>
         <TextField 
           placeholder='Chercher par Nom, Email, N° Tel, N° Réparation, Code Barre...'
